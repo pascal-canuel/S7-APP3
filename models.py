13f33615552c27290cb489cb31ecc61aeb7fea90
Pascal Canuel <canp2403@usherbrooke.ca>
2023-03-17 20:54:56 -0400

    start attention

--- models.py
+++ models.py
@@ -18,6 +18,9 @@ class trajectory2seq(nn.Module):
         self.encoder_layer = nn.GRU(2, hidden_dim, n_layers, batch_first=True)
         self.decoder_layer = nn.GRU(hidden_dim, hidden_dim, n_layers, batch_first=True)
 
+        self.attention_combine = nn.Linear(2*hidden_dim, hidden_dim)
+        self.hidden2query = nn.Linear(hidden_dim, hidden_dim)
+
         self.fc = nn.Linear(hidden_dim, self.dict_size['word'])
         self.to(device)
 
@@ -34,18 +37,54 @@ class trajectory2seq(nn.Module):
 
         for i in range(max_len):
             vec_in.detach()
-            vec_y = self.word_embedding(vec_in)
-            out, hidden = self.decoder_layer(vec_y, hidden)
+            out, hidden = self.decoder_layer(self.word_embedding(vec_in), hidden)
             out = self.fc(out)
             vec_in = torch.argmax(out, dim=2)
             vec_out[:, i, :] = out[:, 0, :]
 
         return vec_out, hidden, None
 
+    def attentionModule(self, query, values):
+        query = self.hidden2query(query)
+
+        dot_product = torch.bmm(values, torch.transpose(query, 1, 2))
+        sm = nn.Softmax(dim=1)
+
+        attention_weights = sm(dot_product)
+
+        attention_output = values * attention_weights
+        attention_output = torch.sum(attention_output, dim=1, keepdim=True)
+
+        return attention_output, attention_weights
+
+    def decoderWithAttention(self, encoder_outs, hidden):
+        max_len = self.maxlen['word']
+        batch_size = hidden.shape[1]
+        vec_in = torch.zeros((batch_size, 1)).to(self.device).long()
+        vec_out = torch.zeros((batch_size, max_len, self.dict_size['word'])).to(self.device)
+        attention_weights = torch.zeros((batch_size, self.maxlen['handwritten'], self.maxlen['word'])).to(self.device)
+
+        for i in range(max_len):
+            vec_in.detach()
+            q, hidden = self.decoder_layer(self.word_embedding(vec_in), hidden)
+
+            a, a_weights = self.attentionModule(q, encoder_outs)
+
+            y = torch.cat((q, a), dim=2)
+            y = self.att_combine(y)
+            out = self.fc(y)
+
+            vec_in = torch.argmax(out, dim=2)
+            vec_out[:, i, :] = out[:, 0, :]
+            attention_weights[:, :, i] = a_weights[:, :, 0]
+
+        return vec_out, hidden, attention_weights
+
     def forward(self, x):
         x = torch.transpose(x, 1, 2)
         out, h = self.encoder(x)
-        out, hidden, attn = self.decoder(out, h)
+        # out, hidden, attn = self.decoder(out, h)
+        out, hidden, attn = self.decoderWithAttention(out, h)
         return out, hidden, attn
     
 
